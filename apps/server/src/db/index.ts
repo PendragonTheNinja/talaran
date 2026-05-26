6a04fdffdf2cf9c6b079f46922467a53ae70b4a9
Pendragon <PendragonTheNinja@gmail.com>
2026-05-26 19:48:57 -0400

    Fix circular dependency in db/index.ts

--- apps/server/src/db/index.ts
+++ apps/server/src/db/index.ts
@@ -1,16 +1,10 @@
 import knex from 'knex';
 import dotenv from 'dotenv';
-import { logger } from '../index';
-
 dotenv.config();
 
 const db = knex({
   client: 'pg',
   connection: process.env.DATABASE_URL,
 });
-
-db.raw('SELECT 1')
-  .then(() => logger.info('Database connected successfully'))
-  .catch((err) => logger.error(`Database connection failed: ${err.message}`));
 
 export default db;
